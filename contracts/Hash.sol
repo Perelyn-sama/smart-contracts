4e280830d4e078e4e55d7d33c58bf8dd6b3db8d3
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2021-12-30 13:10:34 +0100

    encodepacked and encode

--- contracts/Hash.sol
+++ contracts/Hash.sol
@@ -5,4 +5,11 @@ contract HashFunc {
     function hash(string memory text, uint num, address addr) external pure returns(bytes32){
       return  keccak256(abi.encodePacked(text,num, addr));
     }
+
+    function encode(string memory text0, string memory text1 ) external pure returns(bytes memory){
+        return abi.encode(text0, text1);
+    }
+    function encodePacked(string memory text0, string memory text1 ) external pure returns(bytes memory){
+        return abi.encodePacked(text0, text1);
+    }
 }

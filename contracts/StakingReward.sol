120f04df16600547d7913623078f4a128b60d9f3
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2021-12-31 19:29:46 +0100

    clever contract

--- contracts/StakingReward.sol
+++ contracts/StakingReward.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.0 <0.9.0;
+pragma solidity ^0.8;
 
-contract StakingReward{
+contract StakingRewards {
     IERC20 public rewardsToken;
     IERC20 public stakingToken;
 
-    uint public rewardRage = 100;
+    uint public rewardRate = 100;
     uint public lastUpdateTime;
     uint public rewardPerTokenStored;
 
@@ -15,33 +15,46 @@ contract StakingReward{
     uint private _totalSupply;
     mapping(address => uint) private _balances;
 
-    constructor(address _stakingToken, address _rewardsToken){
+    constructor(address _stakingToken, address _rewardsToken) {
         stakingToken = IERC20(_stakingToken);
         rewardsToken = IERC20(_rewardsToken);
     }
 
-    function rewardPerToken() public view returns (uint){
-
+    function rewardPerToken() public view returns (uint) {
+        if (_totalSupply == 0) {
+            return 0;
+        }
+        return
+            rewardPerTokenStored +
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _totalSupply);
     }
 
-    function earned(address account) public view returns (uint){
-
+    function earned(address account) public view returns (uint) {
+        return
+            ((_balances[account] *
+                (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) +
+            rewards[account];
     }
 
-    modifier updateReward(address account){
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = block.timestamp;
+
+        rewards[account] = earned(account);
+        userRewardPerTokenPaid[account] = rewardPerTokenStored;
         _;
     }
 
-    function stake(uint _amount) external updateReward(msg.sender){
+    function stake(uint _amount) external updateReward(msg.sender) {
         _totalSupply += _amount;
         _balances[msg.sender] += _amount;
-        stakingToken.transferFrom(msg.sender,address(this), _amount);
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
     }
 
     function withdraw(uint _amount) external updateReward(msg.sender) {
         _totalSupply -= _amount;
         _balances[msg.sender] -= _amount;
-        stakingToken.tranfer(msg.sender, _amount);
+        stakingToken.transfer(msg.sender, _amount);
     }
 
     function getReward() external updateReward(msg.sender) {
@@ -49,9 +62,9 @@ contract StakingReward{
         rewards[msg.sender] = 0;
         rewardsToken.transfer(msg.sender, reward);
     }
+}
 
-
-    interface IERC20 {
+interface IERC20 {
     function totalSupply() external view returns (uint);
 
     function balanceOf(address account) external view returns (uint);
@@ -70,5 +83,4 @@ contract StakingReward{
 
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
-    }
 }

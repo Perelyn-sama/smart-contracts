c866747aa82bae6ba99672e001f87aa2b9d10ea1
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-04-29 20:26:07 +0100

    factory tut

---
+++ contracts/CounterFactory.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+// https://ethereumdev.io/interact-with-other-contracts-from-solidity/
+pragma solidity 0.5.17;
+
+contract Counter {
+    uint256 private _count;
+    address private _owner;
+    address private _factory;
+
+    modifier onlyOwner(address caller) {
+        require(caller == _owner, "You're not the owner of the contract");
+        _;
+    }
+
+    modifier onlyFactory() {
+        require(msg.sender == _factory, "You need to use the factory");
+        _;
+    }
+
+    constructor(address owner) public {
+        _owner = owner;
+        _factory = msg.sender;
+    }
+
+    function getCount() public view returns (uint256) {
+        return _count;
+    }
+
+    function increment(address caller) public onlyFactory onlyOwner(caller) {
+        _count++;
+    }
+}
+
+/**
+When you call a contract code from another contract, the msg.senderwill refer to the address of our factory 
+
+ */
+
+contract CounterFactory {
+    mapping(address => Counter) _counters;
+
+    function createCounter() public {
+        require(_counters[msg.sender] == Counter(0));
+        _counters[msg.sender] = new Counter(msg.sender);
+    }
+
+    function getCount(address account) public view returns (uint256) {
+        require(_counters[account] != Counter(0));
+        return (_counters[account].getCount());
+    }
+
+    function getMyCount() public view returns (uint256) {
+        return (getCount(msg.sender));
+    }
+
+    function increment() public {
+        require(_counters[msg.sender] != Counter(0));
+        Counter(_counters[msg.sender]).increment(msg.sender);
+    }
+}
